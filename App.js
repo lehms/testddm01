7336a968f28f13a4f772d837beaa17fff4558a81
lehms <leticia.silva2096@etec.sp.gov.br>
2024-02-26 12:06:23 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Desenvolvimento de dispositivo moveis </Text>
+      <Text>Leticia Beatriz Moura da Silva</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
