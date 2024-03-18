8453d6a458c5c47b3a91bca3a57cb96b5184205a
lehms <leticia.silva2096@etec.sp.gov.br>
2024-03-18 12:06:01 -0300

    prova

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Desenvolvimento de dispositivo moveis </Text>
+
       <Text>Leticia Beatriz Moura da Silva</Text>
-      <Text>open up App.js to start working on yur app!</Text>
-      <Text>clone do projeto aula 04-03-2024 3B3 lado A</Text>
-      <Button title='Enviar'></Button>
-      <StatusBar style="auto" />
+
     </View>
   );
 }
